650b5499b0b3aa506b97c884cd2bfba10534f349
Ali <alibahaa786@gmail.com>
2023-09-07 02:41:40 -0700

    Fixed task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import calculator_1 as cal
-a = 10
-b = 5
 if __name__ == "__main__":
+    a = 10
+    b = 5
     print("{} + {} = {}".format(a, b, cal.add(a, b)))
     print("{} - {} = {}".format(a, b, cal.sub(a, b)))
     print("{} * {} = {}".format(a, b, cal.mul(a, b)))
