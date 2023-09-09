968bbae8e19c287ac613ddef9f17c80527b3f8b9
Ali <alibahaa786@gmail.com>
2023-09-09 02:56:47 -0700

    Completed tasks 0-6

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    array_string = [letter for letter in my_string if letter != 'c' and letter != 'C']
+    my_string = ""
+    for letter in array_string:
+        my_string += letter
+    return my_string
