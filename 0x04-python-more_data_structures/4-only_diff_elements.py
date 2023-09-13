5efbf7ab9a35f082e31920f5c261312d0756e3f1
Ali <alibahaa786@gmail.com>
2023-09-13 14:32:54 -0700

    done

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    lst = []
+    check = 0
+    for i in set_1:
+        for num in set_2:
+            if  i == num:
+                check = 1
+                break
+        if not check:
+            lst.append(i)
+        check = 0
+    for i in set_2:
+        for num in set_1:
+            if  i == num:
+                check = 1
+                break
+        if not check:
+            lst.append(i)
+        check = 0
+    ret = {x for x in lst}
+    return ret
